84eb6afc4882d6a5008dbd10250a89dee66992fe
lia carballal <telerik@aerowi.es>
2014-04-10 04:32:14 -0500

    Changes made by lia carballal (telerik@aerowi.es).

--- wiVivo David y Taninos/scripts/wiVivo.js
+++ wiVivo David y Taninos/scripts/wiVivo.js
@@ -278,7 +278,7 @@ function descargaImagen(imagen){
     	rutaImagen,
     	function(entry) {
             document.getElementById("div-resultado-descarga").innerHTML = '';
-            document.getElementById("div-resultado-descarga-ruta").innerHTML += 'Foto gardada en: '+entry.fullPath+'\n';
+            document.getElementById("div-resultado-descarga-ruta").innerHTML += 'Foto gardada en: '+entry.fullPath+'<br/>';
         	//alert("descarga completada: " + entry.fullPath);
     	},
     	function(error) {
